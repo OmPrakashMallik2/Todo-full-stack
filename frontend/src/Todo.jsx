c16b33f798e4b80cb58f85d29ca31c16e78f8e8f
Om Prakash Mallik <opm202000@gmail.com>
2024-03-16 16:12:18 +0530

    Project completed

--- frontend/src/Todo.jsx
+++ frontend/src/Todo.jsx
@@ -1,20 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { FaRegCheckCircle } from "react-icons/fa";
 
 function Todo() {
-
     const [newTaskTitle, setNewTaskTitle] = useState('');
-    const [tasks, setTasks] = useState([
-        {
-            id: 0,
-            title: "Title",
-            completed: false
-        }
-    ]);
+    const [tasks, setTasks] = useState([]);
 
     const handleChange = (e) => {
-        const task = e.target.value;
-        setNewTaskTitle(task);
+        setNewTaskTitle(e.target.value);
     }
 
     const addTask = (taskTitle) => {
@@ -30,13 +24,11 @@ function Todo() {
             })
     }
 
-
     const deleteTask = (taskId) => {
         axios
             .delete("http://localhost:8080/tasks/" + taskId)
             .then((res) => {
                 displayTasks();
-                // console.log("task deleted successfully");
             })
             .catch((err) => {
                 console.error("problem is: " + err);
@@ -48,7 +40,6 @@ function Todo() {
             .put("http://localhost:8080/tasks/" + taskId)
             .then((res) => {
                 displayTasks();
-                // console.log(res.data);
             })
             .catch((err) => {
                 console.error("problem is: " + err);
@@ -60,7 +51,6 @@ function Todo() {
             .get("http://localhost:8080/tasks")
             .then((res) => {
                 setTasks(res.data);
-                // console.log(res.data);
             })
             .catch((err) => {
                 console.error(err);
@@ -72,27 +62,31 @@ function Todo() {
     }, [])
 
     return (
-        <div>
-            <h1 className='text-5xl text-center font-bold text-blue-500 py-5'>Todo full stack project🚀</h1>
-            <form onSubmit={() => addTask(newTaskTitle)} className='bg-purple-500 p-5 flex items-center'>
+        <div className='flex flex-col items-center text-neutral-700'>
+            <h1 className='text-5xl text-center text-white font-bold py-5'>Todo Web App🚀</h1>
+            <form onSubmit={(e) => { e.preventDefault(); addTask(newTaskTitle) }} className='w-full md:w-2/3 lg:w-1/2 p-3 mb-2 rounded bg-white flex flex-col md:flex-row justify-between items-center'>
                 <input
                     type="text"
+                    required
                     onChange={handleChange}
                     placeholder='Type your task...'
-                    className='p-4 w-80 text-lg outline-none rounded'
+                    className='py-3 px-4 mb-2 md:mb-0 w-full md:w-auto text-lg font-semibold outline-none focus:bg-white bg-neutral-200 rounded'
                 />
-                <button type='submit' className='text-xl bg-white ml-5 py-4 px-5 rounded font-bold'>Add</button>
+                <button type='submit' className='text-lg bg-blue-500 hover:bg-blue-600 text-white py-3 px-8 rounded font-bold'>Add</button>
             </form>
-            {tasks.map((task) => (
-                <div key={task.id} className='bg-purple-500 p-5 flex items-center'>
-                    <p className={`p-4 w-80 text-lg text-purple-600 font-semibold rounded ${task.completed ? 'bg-green-200' : 'bg-neutral-400'}`}>{task.title} </p>
-                    <button onClick={() => deleteTask(task.id)} className='text-xl bg-red-500 text-white ml-5 py-4 px-5 rounded font-bold'>Delete</button>
-                    <button onClick={() => doneTask(task.id)} className='text-xl bg-green-500 text-white ml-5 py-4 px-5 rounded font-bold'>Check</button>
-                </div>
-            ))
-            }
+            <div className='w-full md:w-2/3 lg:w-1/2'>
+                {
+                    tasks.map((task) => (
+                        <div key={task.id} className='my-1 gap-2 flex items-center rounded'>
+                            <p className={`py-3 px-4 w-full text-lg font-semibold rounded ${task.completed ? 'bg-green-300' : 'bg-white'}`}>{task.title} </p>
+                            <button onClick={() => deleteTask(task.id)} className='text-xl bg-red-400 hover:bg-red-500 text-white  p-3 rounded-full font-bold'><RiDeleteBin6Line /></button>
+                            <button onClick={() => doneTask(task.id)} className='text-xl bg-green-400 hover:bg-green-500 text-white  p-3 rounded-full font-bold'>< FaRegCheckCircle /></button>
+                        </div>
+                    ))
+                }
+            </div>
         </div >
     )
 }
 
-export default Todo
+export default Todo;
